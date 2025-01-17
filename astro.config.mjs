96bb107e09a9cbcb97353a8b53a0e24f1fb3345c
Andru0Gx <Andy20ogx@gmail.com>
2025-01-16 21:26:04 -0400

    Initialize project with Astro, Tailwind, and database configuration

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import vercel from '@astrojs/vercel';
+
+import db from '@astrojs/db';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), db(), react()],
+  adapter: vercel()
+});

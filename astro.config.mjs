dd6f4ccfb45e8fdb3e0490e881cb93fdad437ecd
Andru0Gx <Andy20ogx@gmail.com>
2025-01-17 21:34:10 -0400

    Refactor configuration files for consistent formatting and remove unused database integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
-import db from '@astrojs/db';
-
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), react()],
-  adapter: vercel()
+    integrations: [tailwind(), react()],
+    adapter: vercel(),
 });
